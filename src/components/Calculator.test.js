2d136ea04ecc9b5d1893b0d896efd60dffa6bb35
emhamza <imumairhamza@gmail.com>
2023-05-04 11:19:20 +0500

    Add fireEvent to calculator.test.js

--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent } from '@testing-library/react';
 import Calculator from './Calculator';
 
 describe('Calculator', () => {
-  it('renders correctly', () => {
-    const { asFragment } = render(<Calculator />);
-    expect(asFragment()).toMatchSnapshot();
+  it('should render the component', () => {
+    const { container } = render(<Calculator />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should update the screen on button click', () => {
+    const { getByTestId, getByText } = render(<Calculator />);
+    fireEvent.click(getByText('2'));
+    fireEvent.click(getByText('+'));
+    fireEvent.click(getByText('3'));
+    fireEvent.click(getByText('='));
+    expect(getByTestId('screen')).toHaveTextContent('5');
   });
 });
